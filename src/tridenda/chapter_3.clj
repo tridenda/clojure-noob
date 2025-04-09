5ff3e6ed5c6928e2764726f40880aaeb782fba26
Tri Denda <tridenda.nk@gmail.com>
2025-04-09 14:08:23 +0700

    big bang

---
+++ src/tridenda/chapter_3.clj
@@ -0,0 +1,48 @@
+(ns tridenda.chapter-3)
+
+(defn hello
+  [x]
+  (println (str "Hello, " x "!")))
+
+(hello "World")
+
+(+ 1 2 3)
+
+(str "It was panda " "in the library " "with a dust butter")
+
+(if true
+  "By Zeus's hammer!"
+  "By Aquaman's trident!")
+
+(if false
+  "By Zeus's hammer!"
+  "By Aquaman's trident!")
+
+(if false
+  "By Odin's Elbow!")
+
+(if true
+  (do (println "Success!")
+      "By Zeus's hammer!")
+  (do (println "Failure!")
+      "By Aquaman's trident!"))
+
+(when true
+  (println "Success!")
+  "abra cadabra")
+
+(nil? 1)
+
+(nil? nil)
+
+(= 1 1)
+
+(= nil nil)
+
+(= 1 2)
+
+(or false nil :large_I_mean_venti :why_cant_I_just_say_large)
+
+(or (= 0 1) (= "yes" "no"))
+
+(or nil)
---
+++ test/tridenda/clojure_noob_test.clj
@@ -0,0 +1,7 @@
+(ns tridenda.clojure-noob-test
+  (:require [clojure.test :refer :all]
+            [tridenda.clojure-noob :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
